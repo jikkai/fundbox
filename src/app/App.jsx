7fa5c3c830dc7c599c9c38f289b1cff19e79507b
Jikkai Xiao <sonne@asaki.me>
2018-07-25 20:00:28 +0800

    update add-panel

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { LocaleProvider } from 'antd'
-import zhCN from 'antd/lib/locale-provider/zh_CN'
+import { Button, Input, Col } from 'antd'
 import FundTable from './components/table'
 import { getFundByCode } from '../utils/http'
 import storage from '../utils/storage'
+
+const record = {
+  key: '',
+  code: '',
+  name: '',
+  cost: '',
+  share: '',
+  price: '',
+  appraisal: ''
+}
 
 export default class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      fund: []
+      fund: [],
+      record
     }
   }
 
@@ -17,25 +27,89 @@ export default class App extends React.Component {
     const storageFund = await storage.get('fund')
     const originFund = storageFund.length ? storageFund : []
 
-    const fund = await Promise.all(
-      originFund.map(async fund => {
-        const remoteFund = await getFundByCode(fund.code)
-        const { share, cost } = fund
-        fund.appraisal = (share * parseFloat(remoteFund.gsz) - cost * share).toFixed(2)
-        return fund
-      })
-    )
+    const fund = await this.freshFund(originFund)
 
     this.setState({
       fund
     })
   }
 
+  async freshFund (originFund) {
+    return await Promise.all(
+      originFund.map(async fund => {
+        const remoteFund = await getFundByCode(fund.code)
+        const { share, cost } = fund
+        fund.appraisal = (share * parseFloat(remoteFund.gsz) - cost * share).toFixed(2)
+        fund.price = remoteFund.gsz
+        fund.name = remoteFund.name
+        return fund
+      })
+    )
+  }
+
+  handleChange (key, event) {
+    const { value } = event.target
+    this.setState({
+      record: {
+        ...this.state.record,
+        [key]: value,
+        key: key === 'code' ? value : this.state.record.key
+      }
+    })
+  }
+
+  async addFund () {
+    if (this.state.record.code && !this.state.fund.find(fund => this.state.record.code === fund.code)) {
+      const fund = await this.freshFund(
+        [this.state.record].concat(this.state.fund)
+      )
+
+      this.setState({
+        fund
+      })
+      this.setState({
+        record
+      })
+    }
+  }
+
+  updateFund() {
+    console.log(this.state.record)
+  }
+
   render() {
     return (
-      <LocaleProvider locale={zhCN}>
-        <FundTable dataSource={this.state.fund} />
-      </LocaleProvider>
+      <main>
+        <section>
+          <Input.Group>
+            <Col span={4}>
+              <Input
+                placeholder="基金代码"
+                value={this.state.record.code}
+                onChange={event => this.handleChange.bind(this, 'code', event)()}
+              />
+            </Col>
+            <Col span={4}>
+              <Input
+                placeholder="成本价"
+                value={this.state.record.cost}
+                onChange={event => this.handleChange.bind(this, 'cost', event)()}
+              />
+            </Col>
+            <Col span={4}>
+              <Input
+                placeholder="持有份额"
+                value={this.state.record.share}
+                onChange={event => this.handleChange.bind(this, 'share', event)()}
+              />
+            </Col>
+            <Col span={4}>
+              <Button type="primary" onClick={this.addFund.bind(this)} style={{ marginBottom: 16 }}>新增</Button>
+            </Col>
+          </Input.Group>
+        </section>
+        <FundTable dataSource={this.state.fund} updateFund={this.updateFund.bind(this)} />
+      </main>
     )
   }
 }
--- src/app/components/table.jsx
+++ src/app/components/table.jsx
@@ -2,50 +2,57 @@ import React from 'react'
 
 import { Table, Divider } from 'antd'
 
-const columns = [
-  {
-    title: '基金代码',
-    dataIndex: 'code',
-    key: 'code'
-  },
-  {
-    title: '成本价',
-    dataIndex: 'cost',
-    key: 'cost'
-  },
-  {
-    title: '持有份额',
-    dataIndex: 'share',
-    key: 'share'
-  },
-  {
-    title: '最新价格',
-    dataIndex: 'price',
-    key: 'price'
-  },
-  {
-    title: '盈亏估算',
-    dataIndex: 'appraisal',
-    key: 'appraisal'
-  },
-  {
-    title: '操作',
-    key: 'action',
-    render: (text, record) => (
-      <span>
-        修改
-        <Divider type="vertical" />
-        删除
-      </span>
-    )
-  }
-]
-
 export default class FundTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      columns: [
+        {
+          title: '基金代码',
+          dataIndex: 'code',
+          key: 'code'
+        },
+        {
+          title: '成本价',
+          dataIndex: 'cost',
+          key: 'cost'
+        },
+        {
+          title: '持有份额',
+          dataIndex: 'share',
+          key: 'share'
+        },
+        {
+          title: '最新价格',
+          dataIndex: 'price',
+          key: 'price'
+        },
+        {
+          title: '盈亏估算',
+          dataIndex: 'appraisal',
+          key: 'appraisal'
+        },
+        {
+          title: '操作',
+          key: 'action',
+          width: 120,
+          render: (text, record) => (
+            <span>
+              <a onClick={this.props.updateFund()}>修改</a>
+              <Divider type="vertical" />
+              <a>删除</a>
+            </span>
+          )
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <Table
-        columns={columns}
+        size="middle"
+        columns={this.state.columns}
         dataSource={this.props.dataSource}
         pagination={false}
       />
